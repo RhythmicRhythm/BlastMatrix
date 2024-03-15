46e38c8ed9c1f9dffce099197273bf5770c0cbb0
RhythmicRhythm <Okubadejorh@gmail.com>
2024-03-15 20:54:07 +0100

    +

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -25,15 +25,15 @@ const Tasks = () => {
           </div>
 
           <div className="w-full px-6 py-8 guardians text-white rounded-lg bg-white bg-opacity-20 ">
-            <h1 className="text-white text-xl mb-6">Completed Missions</h1>
-            <div className="mb-4 flex-col sm:flex-row flex gap-4 justify-between 
-            bg-gradient-to-r from-yellow-400 to-orange-400 px-2 sm:px-8 py-6 text-center">
+            <h1 className="text-white text-sm sm:text-xl mb-6">Completed Missions</h1>
+            <div className="mb-4 flex-col sm:flex-row flex gap-2 justify-between 
+            bg-gradient-to-r from-yellow-400 to-orange-400 px-2 sm:px-8 py-4 text-center">
               <div className="text-center">
-                <h1 className="text-xs sm:text-2xl">Folow Blast Matrix on X</h1>
+                <h1 className="text-xl sm:text-4xl therace">Folow Blast Matrix on X</h1>
               </div>
               <div className="flex flex-col sm:flex-row gap-4">
                 <div className="">
-                  <h2 className="">500PTS</h2>
+                  <h2 className="text-xs sm:text-2xl">500PTS</h2>
                 </div>
                 <div className="">
                   <button className="bg-white text-black w-full text-xs sm:text-2xl px-6 py-2">
